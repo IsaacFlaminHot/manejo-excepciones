cb586a115b9cadf843104974ba570809a253a409
IsaacFlaminHot <l21212024@tectijuana.edu.mx>
2022-05-30 11:08:32 -0700

    manejo-excepciones
    Programa en clase

---
+++ manejo-excepciones/manejo-excepciones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejo_excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            try
+            {
+                Console.Write("¿Cual es el numero entero a ingresar?: ");
+                Numero = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Math.Pow(Numero, 2));
+            }
+
+            catch (FormatException x)
+            {
+                Console.WriteLine(x.Message);
+                Console.ReadKey();
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("El valor ingresado es muy grande.");
+            }
+        }
+    }
+}
